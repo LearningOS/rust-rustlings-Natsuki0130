0e418f661bc2410d415e73326b71d5512c4e9fff
Natsuki0130 <2537338789@qq.com>
2023-04-10 21:17:53 +0800

    +structs

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -2,15 +2,20 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    red: i32,
+    green: i32,
+    blue: i32,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(i32, i32, i32);
 
-#[derive(Debug)]
+#[derive(Debug)] 
+//利用trait中给的Debug来它允许我们以一种对开发者有帮助的方式打印结构体，以便当我们调试代码时能看到它的值。
+//不过我们必须为结构体显式选择这个功能。为此，在结构体定义之前加上外部属性 #[derive(Debug)]
+// {:?}横着输出 or {:#?} 竖着输出
 struct UnitLikeStruct;
 
 #[cfg(test)]
@@ -19,9 +24,11 @@ mod tests {
 
     #[test]
     fn classic_c_structs() {
-        // TODO: Instantiate a classic c struct!
-        // let green =
-
+        let green = ColorClassicStruct{
+            red: 0,
+            green: 255,
+            blue: 0,
+        };
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
         assert_eq!(green.blue, 0);
@@ -29,9 +36,7 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct!
-        // let green =
-
+        let green = ColorTupleStruct(0, 255, 0);
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
         assert_eq!(green.2, 0);
@@ -39,10 +44,8 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
-        let message = format!("{:?}s are fun!", unit_like_struct);
-
+        let unit_like_struct = UnitLikeStruct;
+        let message = format!("{:?}s are fun!", unit_like_struct);// 像是一种填充
         assert_eq!(message, "UnitLikeStructs are fun!");
     }
 }
