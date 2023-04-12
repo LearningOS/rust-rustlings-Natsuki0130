7837a59072744cdafab912fce367a0d4e0584ee5
Natsuki0130 <2537338789@qq.com>
2023-04-12 18:04:50 +0800

    string

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -16,14 +16,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());//slice.to_owned() 将slice的副本作为一个全新分配的String返回
+    string("nice weather".into());// slice.into() 
+    string(format!("Interpolation {}", "Station"));// 加法
+    string_slice(&String::from("abc")[0..1]);//切片 
+    string_slice("  hello there ".trim());// 修剪 从字符串开头和末尾去掉内容（通常是空白符） 其中 slice.trim_left()只省略开头的空白符，而slice.trim_right反之 其返回值是slice的一个子切片
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));//slice.replace(pattern,replacement) 返回以relpacement替换所有匹配pattern的内容之后得到新的string
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());// 大小写转换，返回slice文本
 }
