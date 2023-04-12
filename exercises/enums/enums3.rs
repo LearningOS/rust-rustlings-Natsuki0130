f5323fd5a6e8ec0c9defef544e70b1de2e5887be
Natsuki0130 <2537338789@qq.com>
2023-04-12 13:05:56 +0800

    +enum

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,12 +2,15 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
-
 struct Point {
     x: u8,
     y: u8,
@@ -37,8 +40,13 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
-    }
+        match message {
+            Message::ChangeColor((r, g, b)) => self.change_color((r, g, b)),
+            Message::Echo(str) => self.echo(str),
+            Message::Move(point) => self.move_position(point),
+            Message::Quit => self.quit(),
+        }
+    }//利用match进行控制，选择合适的函数进行赋值
 }
 
 #[cfg(test)]
