c88ae9675b95b89c62f018187b82d2903a3b1d30
Natsuki0130 <2537338789@qq.com>
2023-04-16 14:16:03 +0800

    up

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -7,10 +7,11 @@
 // Consider what you can add to the Licensed trait.
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+//默认情况的选择
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        "Some information".to_string()
+    }
 }
 
 struct SomeSoftware {
